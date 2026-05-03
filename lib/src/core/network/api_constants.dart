0079f1b7de7946a0101c3a353f0b4b6b3b89e99b
lantemir <temiros@mail.ru>
2026-05-03 15:25:24 +0500

    env changing

--- lib/src/core/network/api_constants.dart
+++ lib/src/core/network/api_constants.dart
@@ -5,7 +5,12 @@ class ApiConstants {
   // Реальный телефон: IP моего компьютера (например 192.168.1.5)
   // Dev сервер: https://...com
 
-  static const String baseUrl = 'http://10.0.2.2:8000/api/v1/';
+  //static const String baseUrl = 'http://10.0.2.2:8000/api/v1/';
+
+  static const String baseUrl = String.fromEnvironment(
+    'BASE_URL',
+    defaultValue: 'http://10.0.2.2:8000/api/v1/',
+  );
 
   // Auth — из accounts/urls.py
   static const String login = 'accounts/login/';
